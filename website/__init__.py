050b2a63bd030618b247ffbe3eff96f395f5d58a
syuhas <syuhas22@gmail.com>
2022-08-08 03:30:33 -0400

    did some tooling, css, color adjust and pictures

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 def create_app():
-    application = Flask(__name__)
+    application = Flask(__name__, static_url_path='/static')
     application.config['SECRET KEY'] = 'asdasdasd'
 
     from .views import views
