b188eff1a6a9ef0d19a779704f841bae426f4c2b
syuhas <syuhas22@gmail.com>
2022-11-14 01:46:38 -0500

    slight change to resume and intro

--- website/__init__.py
+++ website/__init__.py
@@ -5,10 +5,10 @@ def create_app():
     application.config['SECRET KEY'] = 'asdasdasd'
 
     from .views import views
-    from .auth import auth
+    from .conn import conn
 
     application.register_blueprint(views, url_prefix='/')
-    """ application.register_blueprint(auth, url_prefix='/') """
+    application.register_blueprint(conn, url_prefix='/')
 
     application.secret_key = '04241985'
 
--- website/conn.py
+++ website/conn.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, session, redirect, url_for
 
-auth = Blueprint('auth', __name__)
+conn = Blueprint('conn', __name__)
 
 
 
